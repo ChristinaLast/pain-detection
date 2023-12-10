75dab2e2c2888f8209ca6e9a3eb5ce4fc1b34c15
lastc <lastc@mit.edu>
2023-12-10 10:35:50 -0500

    adding prediction API

--- pain-detection/pain_detection/predict.py
+++ pain-detection/pain_detection/predict.py
@@ -4,7 +4,8 @@ import tensorflow as tf
 import asyncio
 import time
 import traceback
-
+import cv2
+import mediapipe as mp
 from hume import HumeStreamClient
 from hume.models.config import LanguageConfig
 
@@ -67,32 +68,101 @@ text_stream = [
 ]
 
 
+# Initialize MediaPipe Face Mesh
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh(
+    static_image_mode=False, max_num_faces=1, min_detection_confidence=0.5
+)
+
+
+# Function to extract features
+def extract_features(landmarks):
+    features = {}
+    # Example: Extracting nose coordinates (replace with correct landmark index)
+    nose = landmarks.landmark[1]
+    features["NOSE_X"] = nose.x
+    features["NOSE_Y"] = nose.y
+    features["NOSE_Z"] = nose.z
+    # Head rotation (simplified example, actual calculation is more complex)
+    # Placeholder values used, replace with correct landmark indices
+    head_rotation = landmarks.landmark[2]
+    features["HEAD_ROTATION_X"] = head_rotation.x
+    features["HEAD_ROTATION_Y"] = head_rotation.y
+    features["HEAD_ROTATION_Z"] = head_rotation.z
+    # Mouth openness
+    # Placeholder values used, replace with correct landmark indices
+    upper_lip = landmarks.landmark[13]
+    lower_lip = landmarks.landmark[14]
+    features["MOUTH_VERTICAL"] = lower_lip.y - upper_lip.y
+    # Add logic for other features here
+    return features
+
+
+def preprocess_features(features):
+    # Assuming 'features' is a dictionary of extracted features
+    # Convert to a list in the correct order, matching the input format of the model
+    feature_list = [features[key] for key in sorted(features)]
+    return np.array([feature_list])  # Convert to a numpy array with shape (1, 19)
+
+
 async def main():
     try:
         client = HumeStreamClient("149BtIPH27m8pyrZDYPzcB2GKkbBgDUGD92mq38ftqGIHYSj")
         config = LanguageConfig(granularity="sentence")
         async with client.connect([config]) as socket:
             for text_sample in text_stream:
-                # Simulate real time speaking with a delay
-                time.sleep(0.25 * len(text_sample.split(" ")))
-                result = await socket.send_text(text_sample)
+                # Use asyncio.sleep instead of time.sleep
+                await asyncio.sleep(0.25 * len(text_sample.split(" ")))
+                result = await socket.send_text(
+                    text_sample
+                )  # Ensure proper use of await
                 print(result)
                 emotions = result["language"]["predictions"][0]["emotions"]
+                print(emotions)
 
+                # Ensure emotions is correctly shaped for the model
+                # emotions = np.array(emotions).reshape(1, -1)  # Adjust as necessary
+
+                # Read the video
+                cap = cv2.VideoCapture("man in pain clip.mp4")
+                while cap.isOpened():
+                    success, image = cap.read()
+                    if not success:
+                        break
+                    # Convert the BGR image to RGB
+                    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                    results = face_mesh.process(image)
+                    # Draw the face mesh annotations on the image
+                    if results.multi_face_landmarks:
+                        for face_landmarks in results.multi_face_landmarks:
+                            # Extract features for each frame
+                            frame_features = extract_features(face_landmarks)
+
+                            # Preprocess the features to match the input shape of the model
+                            processed_features = preprocess_features(frame_features)
+
+                            # Make a prediction
+                            prediction = model.predict(processed_features)
+                            print("Prediction:", prediction)
+                            print("Prediction:", prediction)
+                            print(f"\n{text_sample}")
+                            mp.solutions.drawing_utils.draw_landmarks(
+                                image, face_landmarks, mp_face_mesh.FACEMESH_CONTOURS
+                            )
+                    # Display the image (optional)
+                    cv2.imshow(
+                        "MediaPipe Face Mesh", cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                    )
+                    if cv2.waitKey(5) & 0xFF == 27:
+                        break
+                cap.release()
+                cv2.destroyAllWindows()
                 # Make a prediction
-                prediction = model.predict(emotions)
 
-                # Print the prediction
-                print("Prediction:", prediction)
-
-                print(f"\n{text_sample}")
-                # print_emotions(emotions)
-    except Exception:
-        print(traceback.format_exc())
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        traceback.print_exc()
 
 
-# When running the streaming API outside of a Jupyter notebook you do not need these lines.
-# Jupyter has its own async event loop, so this merges main into the Jupyter event loop.
-# To run this sample in a script with asyncio you can use `asyncio.run(main())`
-loop = asyncio.get_event_loop()
-loop.create_task(main())
+# Run the main function in the event loop
+asyncio.run(main())
